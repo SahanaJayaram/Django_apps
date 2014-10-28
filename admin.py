f8b859ce7c1535006f23f7010905cd411eccba65
SahanaJayaram <SahanaJayaram@users.noreply.github.com>
2014-10-28 20:01:36 +0530

    admin.py

---
+++ admin.py
@@ -0,0 +1,12 @@
+Django_apps
+===========
+from django.contrib import admin
+from models import Person
+from models import Login_details
+#from forms import PersonForm
+#from forms import LoginForm
+
+admin.site.register(Person)
+admin.site.register(Login_details)
+# Register your models here.
+
